970df8a68ab70ae7f793eb3438029da80b7ca0de
Youn-Young-Seon <szdbsyyzs@naver.com>
2021-06-16 18:00:14 +0900

    shopping List

--- chap.1/main.js
+++ chap.1/main.js
@@ -2,12 +2,27 @@ const backgroundText = document.querySelector('.bgSpan');
 const backgroundX = document.querySelector('.line.X');
 const backgroundY = document.querySelector('.line.Y');
 const target = document.querySelector('.target');
-document.addEventListener('mousemove', (e) => {
-    backgroundText.textContent = `${e.clientX}px, ${e.clientY}px`;
-    backgroundText.style.top = `${e.clientY}px`;
-    backgroundText.style.left = `${e.clientX}px`;
-    backgroundX.style.top = `${e.clientY}px`;
-    backgroundY.style.left = `${e.clientX}px`;
-    target.style.top = `${e.clientY}px`;
-    target.style.left = `${e.clientX}px`;
+
+window.addEventListener('load', () =>{
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+    
+        backgroundText.textContent = `${x}px, ${y}px`;
+        // backgroundText.style.top = `${e.clientY}px`;
+        // backgroundText.style.left = `${e.clientX}px`;
+        // backgroundX.style.top = `${e.clientY}px`;
+        // backgroundY.style.left = `${e.clientX}px`;
+        // target.style.top = `${e.clientY}px`;
+        // target.style.left = `${e.clientX}px`;
+    
+        backgroundText.style.transform = `translate(${x}px, ${y}px)`;
+        backgroundX.style.transform = `translate(0, ${y}px)`;
+        backgroundY.style.transform = `translate(${x}px, 0)`;
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+    });
 });
